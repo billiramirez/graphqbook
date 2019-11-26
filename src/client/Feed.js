9feda4c0708c3088f70dc801a9649557c3dd1644
Billi Ramirez <ramirezbilly42@outlook.com>
2019-11-26 15:11:15 -0600

    with cache

--- src/client/Feed.js
+++ src/client/Feed.js
@@ -58,7 +58,14 @@ class Feed extends Component {
     return (
       <div className="container">
         <div className="postForm">
-          <Mutation mutation={ADD_POST} refetchQueries={[{ query: GET_POSTS }]}>
+          <Mutation
+            mutation={ADD_POST}
+            update={(store, { data: { addPost } }) => {
+              const data = store.readQuery({ query: GET_POSTS });
+              data.posts.unshift(addPost);
+              store.writeQuery({ query: GET_POSTS, data });
+            }}
+          >
             {addPost => (
               <form
                 onSubmit={e => {
